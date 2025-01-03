f8e626f72fe4956f0a35b19fa271ebae5f2745d2
Serhii <sv.sergius@gmail.com>
2025-01-03 18:52:33 +0200

    internationalization v2

--- src/components/language.tsx
+++ src/components/language.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { setCookie } from 'cookies-next/client'
 import Link from 'next/link'
 import { useParams, usePathname } from 'next/navigation'
 
@@ -7,30 +6,16 @@ const Language: React.FC<any> = props => {
 	const { lang } = useParams<{ lang: string }>()
 	const pathname = usePathname()
 
-	const onClick = async (e: any) => {
-		let expDate = new Date()
-		expDate.setDate(expDate.getDate() + 365)
-		setCookie('NEXT_LOCALE', e.target.lang, { expires: new Date(expDate) })
-	}
-
 	return (
 		<ul className='language'>
 			<li className={props.mobile ? 'language__item-mobile' : 'language__item'}>
-				{lang === 'ru' ? (
-					<span>Рус</span>
-				) : (
-					<Link href={'/ru' + pathname} lang='ru' onClick={onClick}>
-						Рус
-					</Link>
-				)}
+				{lang === 'ru' ? <span>Рус</span> : <Link href={'/ru' + pathname}>Рус</Link>}
 			</li>
 			<li className={props.mobile ? 'language__item-mobile' : 'language__item'}>
 				{lang === 'uk' ? (
 					<span>Укр</span>
 				) : (
-					<Link href={pathname.substring(3)} lang='uk' onClick={onClick}>
-						Укр
-					</Link>
+					<Link href={pathname.length > 3 ? pathname.substring(3) : 'uk'}>Укр</Link>
 				)}
 			</li>
 		</ul>
